4ec557a68be439da1dd66b88de9700cb4ced061b
roman812 <69262254+roman812@users.noreply.github.com>
2022-04-19 20:36:33 +0300

    Add some fix in second window.

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+"""PySimpleGui and keyboard modules are necessary!"""
 import PySimpleGUI as sg
 import keyboard
 from twitch import TwitchChatStream
+
 
 sg.theme('DarkBlack')
 
 
 def do_nothing():
-    """ This function doing nothing)))"""
+    """ This function doing nothing))) """
     pass
 
 
 def start_stream_chat():
     """ Start stream chat in separated window. """
 
-    layout = [[sg.Multiline(size=(150, 150), key='-IN-',
-                            auto_refresh=True, font=('Cooper Black', 16))]]
+    layout = [[sg.Multiline(size=(500, 500), key='-IN-',
+                            auto_refresh=True, font=('Cooper Black', 12))]]
 
     window = sg.Window('', layout, size=(300, 300), alpha_channel=0.5, no_titlebar=True,
                        keep_on_top=True, force_toplevel=True, finalize=True, modal=True)
 
     keyboard.add_hotkey('ctrl+o', do_nothing)
 
+    # Connecting to stream with user credentials. Warning! Translation must be started!
     with TwitchChatStream(username='viaris123',
                           oauth='oauth:wmuzovp81n3hepr096qmvwybx80lxb',
                           verbose=True) as chatstream:
-        chatstream.send_chat_message("First message!")
+        chatstream.send_chat_message("Let's Go!")
         while True:
             rcv = chatstream.twitch_receive_messages()  # receiving here
             if rcv:
-                window['-IN-'].print(rcv)
+                window['-IN-'].print(f"{rcv[0]['username']} : {rcv[0]['message']}")
             event, value = window.read(timeout=100)
 
             if event == sg.WINDOW_CLOSED or keyboard.is_pressed('ctrl+o'):
@@ -37,10 +40,11 @@ def start_stream_chat():
 
 
 def main():
-    layout = [[sg.Button('Start'), sg.Button('Exit')],
-              [sg.Text('Press Ctrl+o to close second window.')]]
+    layout = [[sg.Text('Press Ctrl+o to close second window.')],
+              [sg.Text('Warning! Translation must be started!')],
+              [sg.Button('Start'), sg.Button('Exit')]]
 
-    window = sg.Window('First window', layout, size=(100, 100), finalize=True)
+    window = sg.Window('Twitch Overlay Chat', layout, size=(250, 250), finalize=True)
     while True:
         event, value = window.read(timeout=100)
         if event == 'Exit' or event == sg.WINDOW_CLOSED:
